2d4b48b79769e7bcafe37f2a9c450548e7c5cc8c
Albert Gutierrez <albertgcsula@gmail.com>
2021-11-14 18:14:48 -0800

    fix list keys

--- src/features/search/ResultsList.jsx
+++ src/features/search/ResultsList.jsx
@@ -6,16 +6,16 @@ export default function ResultsList(props) {
   return (
     <div>
       <ul className="list-group">
-        {results.map(result => (
-          <li key={result.imdbID} className="list-group-item">
+        {results.map((result, index) => (
+          <li key={`${result.imdbID}-${index}`} className="list-group-item">
             <div>
               <a href={`https://www.imdb.com/title/${result.imdbID}/`} rel="noreferrer" target="_blank">
                 <img
                   className="float-start img-fluid me-2"
-                  src={result.Poster} 
+                  src={result.Poster}
                   alt={result.Title}
-                  height="100" 
-                  width="65" 
+                  height="100"
+                  width="65"
                 />
               </a>
               <p className="fw-bold mb-1">{result.Title} ({result.Year})</p>
